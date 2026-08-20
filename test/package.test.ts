9ab3314d87160f0c5989ecda1520d575d1ea6390
Zach Williams <hey@zachwill.com>
2026-08-20 16:11:37 -0700

    Release v0.9.0

--- test/package.test.ts
+++ test/package.test.ts
@@ -112,7 +112,7 @@ describe("published package resources", () => {
     const manifest = await readManifest();
 
     expect(manifest.name).toBe("@zachwill/pi-orchestrate");
-    expect(manifest.version).toBe("0.8.0");
+    expect(manifest.version).toBe("0.9.0");
     expect(manifest.files).toEqual(["extension/", "examples/", "README.md", "LICENSE"]);
     expect(manifest.pi).toEqual({ extensions: ["./extension/index.ts"] });
     expect(manifest.pi.skills).toBeUndefined();
